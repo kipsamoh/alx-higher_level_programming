4c6403fdc3a46b090754f529a6b027db42b1e793
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-14 19:03:15 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- script 8
--- script that creates a table second_table in the database hbtn_0c_0 in
--- your MySQL server and add multiples rows.
+-- script that c_reates a table second_table in the d_atabase hbtn_0c_0 in
+-- your _MySQL server and add _multiples rows.
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
