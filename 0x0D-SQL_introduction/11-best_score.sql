5afe9af70daa68782f22c0cbf8805e45343dca4f
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-14 18:56:10 +0300

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- script 11
--- script that lists all records with a score >= 10 in the table
--- second_table of the database hbtn_0c_0 in your MySQL server.
+-- script _that lists all reco_rds with a s_core >= 10 in the ta_ble
+-- second_table of the d_atabase _hbtn_0c_0 in your _MySQL server.
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
