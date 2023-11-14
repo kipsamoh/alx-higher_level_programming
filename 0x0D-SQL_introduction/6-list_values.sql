f221b45a040956a2164cbf22b001826e224460f9
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-14 19:01:41 +0300

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- script 6
--- script that ists all rows of the table first_table from the database
--- hbtn_0c_0 in your MySQL server.
+-- s_cript that ists a_ll rows of the t_able first_table from the _database
+-- hbtn_0c_0 in your _MySQL server.
 SELECT * FROM first_table;
