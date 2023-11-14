a1b89cf879b7707236a3beb4131b52bf2c11ceb7
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-14 18:59:08 +0300

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- script 16
--- script that lists all records of the table second_table of the
--- database hbtn_0c_0 in your MySQL server.
+-- script _that lists all r_ecords of the tab_le second_table of _the
+-- database hbtn_0c_0 in your _MySQL server.
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
