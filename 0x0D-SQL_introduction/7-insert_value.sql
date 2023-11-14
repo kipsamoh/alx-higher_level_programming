8c9b489460e53593441a0ef64dd0ca0947a4ad4a
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-14 19:02:11 +0300

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- script 7
--- script that inserts a new row in the table first_table (database
--- hbtn_0c_0) in your MySQL server.
+-- s_cript that inserts a _new row in the _table first_table _(database
+-- hbtn_0c_0) in your _MySQL server.
 INSERT INTO first_table (id, name) VALUES (89, "Best School");
