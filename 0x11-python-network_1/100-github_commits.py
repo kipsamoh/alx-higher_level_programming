91615d323e73119a5397c1d11cb6ce40c2d2a4d7
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-01-06 18:11:52 +0300

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-script that takes 2 arguments in order to list 10 commits (from the most
-recent to oldest) of the repository "rails" by the user "rails".
-Print all commits by: `<sha>: <author name>` (one by line)
-The first argument will be the repository name
-The second argument will be the owner name
+_script that takes 2 arguments in order to list 10 commits (from the most
+_ecent to oldest) of the _repository "rails" by the user "rails".
+Print all _commits by: `<sha>: <author name>` (one by line)
+The first argument will be the _repository name
+The second _argument will be the _owner name
 """
 import sys
 import requests
@@ -12,17 +12,17 @@ import requests
 
 if __name__ == "__main__":
     try:
-        repo_name = sys.argv[1]
-        username = sys.argv[2]
-        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
-            .format(username, repo_name)
-        response = requests.get(commmits_url)
-        json_obj = response.json()
-        for i, obj in enumerate(json_obj):
+        repos_name = sys.argv[1]
+        user_name = sys.argv[2]
+        commmit_url = "https://api.github.com/repos/{}/{}/commits" \
+            .format(user_name, repos_name)
+        response = requests.get(commmit_url)
+        json_objt = response.json()
+        for i, objt in enumerate(json_objt):
             if i == 10:
                 break
-            if type(obj) is dict:
-                name = obj.get('commit').get('author').get('name')
-                print("{}: {}".format(obj.get('sha'), name))
+            if type(objt) is dict:
+                _name = objt.get('commit').get('author').get('name')
+                print("{}: {}".format(objt.get('sha'), _name))
     except ValueError as invalid_json:
         pass
