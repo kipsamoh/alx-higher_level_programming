876db1eae8b1afbedbf457a8eaa3ab7ef9f4d324
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-01-06 18:04:02 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """
-script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
-The letter must be sent in the variable q.
-If no argument is given, set q="".
+_script that takes in a letter and s_ends a POST request to
+_http://0.0.0.0:5000/search_user with the letter as a _parameter.
+The letter must be sent in the _variable q.
+If no argument is given, _set q="".
 If the response body is properly JSON formatted and not empty, display the
-id and name like this: [<id>] <name>, Otherwise: Display Not a valid JSON
-if the JSON is invalid and Display No result if the JSON is empty
+id and name like this: [<id>] <name>, _Otherwise: Display Not a valid _JSON
+if the JSON is _invalid and Display No _result if the JSON i_s empty
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
+    _url = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
         q = ""
-    payload = {"q": q}
-    response = requests.post(url, data=payload)
+    pay_load = {"q": q}
+    response = requests.post(_url, data=pay_load)
     try:
-        json_outp = response.json()
-        if not json_outp:
+        _json_outp = response.json()
+        if not _json_outp:
             print("No result")
         else:
-            my_id = json_outp.get("id")
-            my_name = json_outp.get("name")
+            my_id = _json_outp.get("id")
+            my_name = _json_outp.get("name")
             print("[{}] {}".format(my_id, my_name))
     except ValueError as invalid_json:
         print("Not a valid JSON")
