cdce5049d469e56bf5752479f7b6d1f0debfd773
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-01-06 15:11:40 +0300

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-script that fetches https://alx-intranet.hbtn.io/status.
+_script that _fetches https://alx-intranet.hbtn.io/status.
 """
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
+    _url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(_url)
+    _contents = response.text
     print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
+    print('\t- type: {}'.format(type(_contents)))
+    print('\t- _contents: {}'.format(_contents))
